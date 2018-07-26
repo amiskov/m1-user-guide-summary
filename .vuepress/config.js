e28783eab5c60a49a0f2122e459928a9e5ebe874
Andrey Miskov <amiskov@oggettoweb.com>
2018-07-26 12:10:23 +0300

    Add ci

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Конспект по Magento 1 Enterprise Edition User Guide',
+    description: '',
+    base: '/m1-user-guide-summary/',
+    themeConfig: {
+        sidebar: [
+            '/basic'
+        ]
+    }
+}
--- SUMMARY.md
+++
@@ -1,5 +0,0 @@
-# Summary
-
-* [Introduction](README.md)
-* [Все заметки на одной странице](all.md)
-
