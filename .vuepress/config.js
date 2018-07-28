7f9c9e44473fa732c08ee97a32cd3925df087d52
Nerechka <32274153+Nerechka@users.noreply.github.com>
2018-07-29 00:48:19 +0300

    add favicon in config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    head: [
+        ['link', { rel: 'icon', href: 'https://raw.githubusercontent.com/oggetto-academy/m1-user-guide-summary/master/favicon.ico'}]
+    ],
     title: 'Конспект по Magento 1 Enterprise Edition User Guide',
     description: '',
     base: '/m1-user-guide-summary/',
