d2267793185a11f6a630ec2278e0df0c6c9c87f6
nerechka <aimaikina@oggettoweb.com>
2018-08-05 15:06:16 +0300

    update config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,16 +1,35 @@
 module.exports = {
     head: [
-        ['link', { rel: 'icon', href: 'https://raw.githubusercontent.com/oggetto-academy/m1-user-guide-summary/master/favicon.ico'}]
+        ['link', {
+            rel: 'icon',
+            href: 'https://raw.githubusercontent.com/oggetto-academy/m1-user-guide-summary/master/favicon.ico'
+        }]
     ],
     title: 'Конспект по Magento 1',
     description: '',
     base: '/m1-user-guide-summary/',
     themeConfig: {
-        sidebarDepth: 2,
-        displayAllHeaders: true,
-        sidebar: [
-            '/basic',
-            '/product-catalog'
+        sidebar: [{
+            title: 'Basic Configuration',
+            collapsable: true,
+            children: [
+                'basic/store_admin',
+                'basic/store_details',
+                'basic/web_stores_views',
+                'basic/industry_compliance'
+            ]
+        },
+            {
+                title: 'Product Catalog',
+                collapsable: true,
+                children: [
+                    'product/navigation',
+                    'product/categories',
+                    'product/managing_attributes'
+                ]
+            }
         ]
     }
 }
+
+
